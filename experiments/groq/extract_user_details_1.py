c6f2911b6abf5f02cbf639253478f00c61a54a38
Zbigniew Lukasiak <zzbbyy@gmail.com>
2024-05-08 14:18:58 +0200

    With Llama you need to be more precise when pointing to a function

--- experiments/groq/extract_user_details_1.py
+++ experiments/groq/extract_user_details_1.py
@@ -1,22 +1,24 @@
-from llm_easy_tools import get_tool_defs, process_response
+from llm_easy_tools import get_tool_defs, process_response 
+from llm_easy_tools.schema_generator import get_name, llm_function
+
 from pydantic import BaseModel
+from openai import OpenAI
 from pprint import pprint
 
 from groq import Groq
 client = Groq()
 
-
-# Define a Pydantic model for your tool's output
+# with Llama the function name needs to be better fit the action
+@llm_function("extract_user_details")
 class UserDetail(BaseModel):
     name: str
     city: str
-
 
 response = client.chat.completions.create(
     model="llama3-8b-8192",
     messages=[{"role": "user", "content": "Extract user details from the following sentence: John lives in Warsaw and likes banana"}],
     tools=get_tool_defs([UserDetail]),
-    tool_choice={'type': 'function', 'function': {'name': 'UserDetail'}},
+    tool_choice="auto"
 )
 # There might be more than one tool calls in a single response so results are a list
 results = process_response(response, [UserDetail])
