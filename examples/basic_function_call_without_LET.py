70c225e370fd0ebb7204eae4665ecb953f4a4485
Zbigniew Lukasiak <zzbbyy@gmail.com>
2024-09-27 20:35:48 +0200

    motivational example

---
+++ examples/basic_function_call_without_LET.py
@@ -0,0 +1,56 @@
+from llm_easy_tools import get_tool_defs, process_response
+from openai import OpenAI
+from pprint import pprint
+import json
+
+client = OpenAI()
+# This assumes that OpenAI credentials are in the environment variable OPENAI_API_KEY
+
+def contact_user(name: str, city: str) -> str:
+    return f"User {name} from {city} was contacted"
+
+# type annotations are crucial here
+
+tool_schemas = [
+    {
+        "function": {
+            "description": "",
+            "name": "contact_user",
+            "parameters": {
+                "properties": {
+                    "city": {"type": "string"},
+                    "name": {"type": "string"}
+                },
+                "required": ["name", "city"],
+                "type": "object",
+            },
+        },
+        "type": "function",
+    }
+]
+
+text = "John lives in Warsaw. Bob lives in London."
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo-1106",
+    messages=[
+        {"role": "system", "content": "You are a personal assistant. Your current task is to contact all users mentioned in the message."},
+        {"role": "user", "content": text}],
+    tools=tool_schemas,
+    tool_choice="auto",
+)
+
+# you need to loop over all tool calls and process them one by one
+for tool_call in response.choices[0].message.tool_calls:
+    # you need to get the function object from the global namespace
+    function = globals()[tool_call.function.name]
+    # and then call it with the arguments from the tool call
+    function_args = json.loads(tool_call.function.arguments)
+    # and then call it with the arguments from the tool call
+    result = function(**function_args)
+    print(f"Result of function call: {result}")
+
+
+# OUTPUT:
+# 'User John from Warsaw was contacted'
+# 'User Bob from London was contacted'
